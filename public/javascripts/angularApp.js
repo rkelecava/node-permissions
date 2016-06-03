958e89d1d12bd76aa5883a6fba78e5554654dd12
rkelecava <rkelecava@scottelectricusa.com>
2016-06-03 12:45:10 -0400

    Added auth controller to angular

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -1,5 +1,53 @@
 var app = angular.module('app',['ui.router']);
 
+app.factory('auth', [
+	'$http',
+	'$window',
+	function ($http, $window) {
+		var auth = {};
+
+		auth.saveToken = function (token) {
+			$window.localStorage['acl-token'] = token;
+		};
+
+		auth.getToken = function () {
+			return $window.localStorage['acl-token'];
+		};
+
+		auth.isLoggedIn = function () {
+			var token = auth.getToken();
+
+			if (token) {
+				var payload = JSON.parse($window.atob(token.split('.')[1]));
+
+				return payload.exp > Date.now() / 1000;
+			} else {
+				return false;
+			}
+		};
+
+		auth.currentUser = function () {
+			if (auth.isLoggedIn()) {
+				var token = auth.getToken();
+				var payload = JSON.parse($window.atob(token.split('.')[1]));
+
+				return payload.username;
+			}
+		};
+
+		auth.logIn = function (user) {
+			return $http.post('/users/login', user)
+				.success(function (data) {
+					auth.saveToken(data.token);
+				});
+		};
+
+		auth.logOut = function () {
+			$window.localStorage.removeItem('acl-token');
+		};
+	}
+]);
+
 app.config([
 	'$stateProvider',
 	'$urlRouterProvider',
