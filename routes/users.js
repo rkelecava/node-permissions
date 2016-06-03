be4948b807f97f71496cb4f72c94c18d46f2ba8f
rkelecava <rkelecava@scottelectricusa.com>
2016-06-03 12:48:43 -0400

    Added /login route to users.js

--- routes/users.js
+++ routes/users.js
@@ -231,6 +231,29 @@ router.put('/:user/roles', function (req, res, next) {
 	});
 });
 
+/************************/
+/* Route to login a user */
+/**************************/
+router.post('/login', function (req, res, next) {
+	if (!req.body.username || !req.body.password) {
+		return res.status(400).json({
+			message: 'Please fill out all fields'
+		});
+	}
+
+	passport.authenticate('local', function (err, user, info) {
+		if (err) { return next(err); }
+
+		if (user) {
+			return res.json({
+				token: user.generateJWT()
+			});
+		} else {
+			return res.status(401).json(info);
+		}
+	})(req, res, next);
+});
+
 /**************************/
 /* End of Routes */
 /**************************/
